61be55e56d6acb289ec4867f94136e9ce8721252
stephaniaoz <stephania.osorio@correounivalle.edu.co>
2016-09-29 00:13:34 -0500

    se realizan modificiones para generar pestañas o tabs y en ellas visualizar las imágenes, se crean diseños para las pestañas

--- app/src/main/java/univalleapi/tutorial/com/univalleapi/MainActivity.java
+++ app/src/main/java/univalleapi/tutorial/com/univalleapi/MainActivity.java
@@ -1,13 +1,36 @@
 package univalleapi.tutorial.com.univalleapi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnInicioApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnInicioApi = (Button) findViewById(R.id.btnInicio);
+
+        btnInicioApi.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                openContenidoUniversidad();
+            }
+        });
+
+    }
+
+    public void openContenidoUniversidad(){
+
+        Intent i = new Intent(this, MenuOpciones.class);
+        startActivity(i);
+
     }
 }
---
+++ app/src/main/java/univalleapi/tutorial/com/univalleapi/MenuOpciones.java
@@ -0,0 +1,56 @@
+package univalleapi.tutorial.com.univalleapi;
+
+import android.app.Activity;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TabHost;
+
+/**
+ * Created by ADMIN on 28/09/2016.
+ */
+
+public class MenuOpciones extends Activity{
+
+    private TabHost tabsContainer;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        getPropiedadesTab();
+    }
+
+    public void getPropiedadesTab(){
+
+        Resources res = getResources();
+
+        tabsContainer = (TabHost) findViewById(android.R.id.tabhost);
+        tabsContainer.setup();
+
+        TabHost.TabSpec spec = tabsContainer.newTabSpec("mitab1");
+        spec.setContent(R.id.tab1);
+        //spec.setIndicator("CAMPUS", res.getDrawable(android.R.drawable.ic_btn_speak_now));
+        spec.setIndicator("", res.getDrawable(R.drawable.ic_campus));
+        tabsContainer.addTab(spec);
+
+        spec = tabsContainer.newTabSpec("mitab2");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("", res.getDrawable(R.drawable.ic_eventos));
+        tabsContainer.addTab(spec);
+
+        tabsContainer.setCurrentTab(0);
+
+        tabsContainer.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+                Log.i("AndroidTabsDemo", "Pulsada pestaña: " + tabId);
+            }
+
+        });
+
+    }
+
+}
